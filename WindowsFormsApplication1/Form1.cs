8aeef9c7f5e113d17ecbed223d9b56ab66187836
unknown <toyou.dev@gmail.com>
2015-08-12 22:47:00 -0700

    Initialization

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using  MysqlConnector;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string name,address;
+        int age;
+        DBConnect db;
+        ListView listView1;
+        ListViewItem itm;
+        
+        TextBox textBox1,textBox2,textBox3;
+        public Form1()
+        {
+           
+           InitializeComponent();
+           db  = new DBConnect();
+        }
+
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1= new TextBox();
+            textBox1.Location= new Point(200,0);
+            textBox1.Width = 100;
+            textBox1.Multiline = true;
+            textBox1.BackColor = Color.White;
+            textBox1.ForeColor = Color.Black;
+            textBox1.BorderStyle = BorderStyle.Fixed3D;
+            textBox1.Text="enter name";
+            Controls.Add(textBox1);
+
+            textBox2 = new TextBox();
+            textBox2.Location= new Point(200,40);
+            textBox2.Text = "enter age";
+            Controls.Add(textBox2);
+
+             textBox3 = new TextBox();
+            textBox3.Location= new Point(200,80);
+            textBox3.Text = "Address";
+            Controls.Add(textBox3);
+
+            Button insertbutton = new Button();
+            insertbutton.Text = "Submit";
+            insertbutton.Location=new Point(200,100);
+            insertbutton.Click += new EventHandler(insertbutton_Click);
+            Controls.Add(insertbutton);
+
+            Button displaybutton = new Button();
+            displaybutton.Text = "Display";
+            displaybutton.Location = new Point(200, 200);
+            displaybutton.Click += new EventHandler(displaybutton_Click);
+            Controls.Add(displaybutton);
+
+            listView1 = new ListView();
+            listView1.Size = new Size(200, 400);
+            listView1.Columns.Add("Name", 70);
+            listView1.Columns.Add("Age", 10);
+            listView1.Columns.Add("Address", 70);
+            listView1.View = View.Details;
+            Controls.Add(listView1);
+            
+        }
+
+         void insertbutton_Click(object sender, EventArgs e)
+        {
+            age = Int32.Parse(textBox2.Text);
+            db.Insert(textBox1.Text,age,textBox3.Text); 
+            MessageBox.Show("");
+        }
+
+
+         void displaybutton_Click(object sender, EventArgs e)
+         {
+             //Add items in the listview
+             string[] arr = new string[4];
+             //Add column header
+            
+             listView1.Items.Clear();
+             List<string> list = new List<string>();
+             list = db.GetCustomerInfo();
+             for (int row = 0; row < list.Count; row = row + 3)
+             {
+                 arr[0] = list[row];
+                 arr[1] = list[row + 1];
+                 arr[2] = list[row + 2];
+                 Console.WriteLine(list[row]+list[row+1]+list[row+2]);
+                 itm = new ListViewItem(arr);
+                 listView1.Items.Add(itm);
+
+                 
+             }
+
+             
+         }
+    }
+}
---
+++ WindowsFormsApplication1/Mysqlconnector.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//Add MySql Library
+using MySql.Data.MySqlClient;
+
+namespace MysqlConnector
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+            OpenConnection();
+            
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "company_employee";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("connection established");
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+
+        //Insert statement
+        public void Insert(string name,int age,string address)
+        {
+            
+            string cmdText = "INSERT INTO  employeinfo(name,age,address) VALUES (@person, @personage,@personaddress)";
+            MySqlCommand cmd = new MySqlCommand(cmdText, connection);
+            cmd.Parameters.AddWithValue("@person", name);
+            cmd.Parameters.AddWithValue("@personage", age);
+            cmd.Parameters.AddWithValue("@personaddress", address);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            dataReader.Close();
+        }
+
+        //Update statement
+        public void Update()
+        {
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+        }
+
+        //Select statement
+        public List<string>GetCustomerInfo()
+        {
+            string query = "SELECT * FROM employeinfo";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+            List<string> datalist = new List<string>();
+            
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                int i = 0;
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["name"] + "");
+                    list[1].Add(dataReader["age"] + "");
+                    list[2].Add(dataReader["address"] + "");
+                    
+                    datalist.Add(dataReader["name"] + "");
+                   
+                    datalist.Add(dataReader["age"] + ""); 
+                    datalist.Add(dataReader["address"] + ""); 
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                
+                //close Connection
+                //this.CloseConnection();
+
+                //return list to be displayed
+                return datalist;
+           
+
+        }
+
+        //Count statement
+        // public int Count()
+        //{
+        //}
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
+        {
+        }
+    }
+
+}
